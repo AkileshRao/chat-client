c026a60a884d753c493bc0e14f7c3ff0241f7782
Akilesh Rao <akilesh.rao@routemobile.com>
2021-01-21 14:32:23 +0530

    Chat page changes

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -5,6 +5,12 @@ import { SocketContext } from '../../socketContext'
 import { Box, Flex, Heading, IconButton, Input, Text } from "@chakra-ui/react"
 import { FiLogOut } from 'react-icons/fi'
 import { BiMessageDetail } from 'react-icons/bi'
+import { RiSendPlaneFill } from 'react-icons/ri'
+import ScrollToBottom from 'react-scroll-to-bottom';
+
+import './Chat.scss'
+
+
 
 const Chat = () => {
     const { name, room, setName, setRoom } = useContext(MainContext)
@@ -42,28 +48,34 @@ const Chat = () => {
 
     return (
         <Flex className='room' flexDirection='column' width={{ base: "80%", md: '575px' }}>
-            <Heading as='h1' bg='white' p='1rem'>
+            <Heading className='heading' as='h1' bg='white' p='1rem' borderRadius='10px 10px 0 0'>
                 <Flex alignItems='center' justifyContent='space-between'>
                     {room}
-                    <IconButton icon={<FiLogOut />} onClick={logout} colorScheme='red' />
+                    <Box w={5} h={5} onClick={logout} bg='tomato' borderRadius='100px' cursor='pointer' />
                 </Flex>
             </Heading>
-            <Flex className='messages' minHeight='400px' bg='#EAEAEA' justifyContent='center' alignItems='center'>
+
+
+            <ScrollToBottom className='messages' >
                 {messages.length > 0 ?
                     messages.map((msg, i) =>
-                        (<p key={i}>{msg.text} by {msg.user}</p>)
+                    (<Box key={i} className={`message ${msg.user === name ? "my-message" : ""}`} m=".2rem 0">
+                        <Text fontSize='xs' opacity='.7' ml='5px' className='user'>{msg.user}</Text>
+                        <Text className='msg' p=".4rem .8rem" bg='white' borderRadius='15px' letterSpacing='-1px' color='white'>{msg.text}</Text>
+                    </Box>)
                     )
                     :
-                    <Flex alignItems='center' bg='#EAEAEA' opacity='.2'>
-                        <BiMessageDetail fontSize='3rem' />
-                        <Text ml='2' fontSize='4xl' fontWeight='500'>No messages</Text>
+                    <Flex alignItems='center' justifyContent='center' bg='#EAEAEA' opacity='.2'>
+                        <Box mr='2'>-----</Box>
+                        <BiMessageDetail fontSize='1rem' />
+                        <Text ml='1' fontWeight='400'>No messages</Text>
+                        <Box ml='2'>-----</Box>
                     </Flex>
                 }
-
-            </Flex>
-            <div>
+            </ScrollToBottom>
+            <div className='form'>
                 <input type="text" placeholder='Enter Message' value={message} onChange={e => setMessage(e.target.value)} />
-                <button onClick={handleSendMessage}>Send</button>
+                <IconButton colorScheme='green' isRound='true' icon={<RiSendPlaneFill />} onClick={handleSendMessage}>Send</IconButton>
             </div>
         </Flex>
     )
